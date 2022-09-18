bf58d3969a75088b71c901fda65671f33e5424c7
Anna Persh <dearest.gino@gmail.com>
2022-09-18 15:44:48 +0500

    Initial commit

---
+++ src/javaBasic02/main.java
@@ -0,0 +1,70 @@
+package javaBasic02;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь до файла:");
+        //Введите путь, например C:\Users\User_name\path...\src\javaBasic02\input.txt
+        //В файле nput.txt есть пример ввода
+        String input = scanner.nextLine().strip();
+        String file;
+        try {
+            file = Files.readString(Path.of(input));
+        } catch (IOException exception) {
+            System.out.println("Ошибка: файл не найден или путь введён неправильно" + exception);
+            return;
+        }
+        String[] words = file.split("[^a-zA-Z]");
+        TreeMap<String, Integer> wordStat = new TreeMap<>();
+        int wordsAmount = 0;
+        for (String word : words) {
+            if (word.isBlank()) {
+                continue;
+            }
+            wordsAmount++;
+            String lowercaseWord = word.toLowerCase();
+            Integer wordCount = wordStat.getOrDefault(lowercaseWord, 0);
+            wordStat.put(lowercaseWord, wordCount + 1);
+        }
+        if (wordStat.isEmpty()) {
+            System.out.println("Ошибка: в файле нет слов!");
+            return;
+        }
+
+        System.out.println("Слова в файле:");
+        System.out.println(wordStat.keySet());
+
+        System.out.println("Всего слов в файле: " + wordsAmount);
+        System.out.println();
+        System.out.println("Статистика:");
+        for (Map.Entry<String, Integer> entry : wordStat.entrySet()) {
+            String word = entry.getKey();
+            Integer count = entry.getValue();
+            System.out.printf("%s:      количество - %3d, частота - %.2f%%\n", word, count, (count/(double)wordsAmount));
+        }
+
+        TreeSet<String> maxWords = new TreeSet<>();
+        int max = -1;
+        for (String word : wordStat.keySet()) {
+            Integer wordCount = wordStat.get(word);
+            if (wordCount > max) {
+                max = wordCount;
+                maxWords.clear();
+                maxWords.add(word);
+            } else if (wordCount == max) {
+                maxWords.add(word);
+            }
+            
+        }
+        System.out.println();
+        System.out.println("Чаще всего встречались слова: ");
+        System.out.println(maxWords);
+    }
+}
